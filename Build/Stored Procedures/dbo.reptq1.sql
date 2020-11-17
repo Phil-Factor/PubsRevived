a865a0fbf47b0d2f73c54b302415cb542236d8a6
Phil Factor <Phil.Factor@simple-talk.com>
2020-11-17 11:50:26 +0000

    Updated reptq1 and change to titles

--- Build/Stored Procedures/dbo.reptq1.sql
+++ Build/Stored Procedures/dbo.reptq1.sql
@@ -2,15 +2,21 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-
-CREATE PROCEDURE [dbo].[reptq1]
+CREATE   PROCEDURE [dbo].[reptq1]
 AS
   BEGIN
-    SELECT CASE WHEN Grouping(titles.pub_id) = 1 THEN 'ALL' ELSE titles.pub_id END AS pub_id,
-      Avg(titles.price) AS avg_price
-      FROM dbo.titles AS titles
-      WHERE titles.price IS NOT NULL
-      GROUP BY titles.pub_id WITH ROLLUP
-      ORDER BY pub_id;
+    SELECT CASE WHEN Grouping(publications.pub_id) = 1 
+	         THEN 'ALL' ELSE publications.pub_id END AS pub_id,
+      Avg(price) AS avg_price
+      FROM dbo.publishers
+        INNER JOIN dbo.publications
+          ON publications.pub_id = publishers.pub_id
+        INNER JOIN editions
+          ON editions.publication_id = publications.Publication_id
+        INNER JOIN dbo.prices
+          ON prices.Edition_id = editions.Edition_id
+      WHERE prices.PriceEndDate IS NULL
+      GROUP BY publications.pub_id WITH ROLLUP
+      ORDER BY publications.pub_id;
   END;
 GO
--- Build/Views/dbo.titles.sql
+++ Build/Views/dbo.titles.sql
@@ -2,10 +2,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-Create view [dbo].[titles]
+Create   VIEW [dbo].[titles]
 /* this view replaces the old TITLES table and shows only those books that represent each publication and only the current price */
 as
-SELECT publications.Publication_id AS title_id, publications.title, pub_id,
+SELECT publications.Publication_id AS title_id, publications.title, Tag AS [Type], pub_id,
   price, advance, royalty, ytd_sales, notes, pubdate
   FROM publications
     INNER JOIN editions
@@ -13,5 +13,10 @@ SELECT publications.Publication_id AS title_id, publications.title, pub_id,
      AND Publication_type = 'book'
     INNER JOIN prices
       ON prices.Edition_id = editions.Edition_id
+	LEFT OUTER JOIN TagTitle
+      ON TagTitle.title_id = publications.Publication_id
+     AND TagTitle.Is_Primary = 1--just the first, primary, tag
+	     LEFT OUTER JOIN dbo.TagName
+      ON TagTitle.TagName_ID = TagName.TagName_ID
   WHERE prices.PriceEndDate IS NULL;
 GO
